136ea1bd1fa8fb6f24beb7dafc438d57d10587df
kev-karan <kevkaran@proton.me>
2026-05-07 17:05:56 -0300

    feat: add Fluxograma model and register in admin, update settings

--- projeto_genesis/fluxogramas/models.py
+++ projeto_genesis/fluxogramas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Fluxograma(models.Model):
+    titulo = models.CharField(max_length=200)
+    descricao = models.TextField(blank=True, null=True)
+    conteudo = models.JSONField(help_text="Estrutura JSON do fluxo")
+    
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
