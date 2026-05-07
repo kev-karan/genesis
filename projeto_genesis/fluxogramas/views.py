cb5f2727b8bccc6ddfd0c128e7bc70c8f125181e
kev-karan <kevkaran@proton.me>
2026-05-07 17:32:10 -0300

    feat: add API endpoints for Fluxogramas with listing and detail views

--- projeto_genesis/fluxogramas/views.py
+++ projeto_genesis/fluxogramas/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from django.db.models import Q
+from .models import Fluxograma
 
-# Create your views here.
+def listar_e_buscar_fluxogramas(request):
+    termo_busca = request.GET.get('busca', '')
+
+    if termo_busca:
+        fluxogramas = Fluxograma.objects.filter(
+            Q(titulo__icontains=termo_busca) | Q(descricao__icontains=termo_busca)
+        )
+    else:
+        fluxogramas = Fluxograma.objects.all()
+
+    dados = []
+    for fluxo in fluxogramas:
+        dados.append({
+            'id': fluxo.id,
+            'titulo': fluxo.titulo,
+            'descricao': fluxo.descricao
+        })
+
+    return JsonResponse({'resultados': dados}, safe=False)
+
+
+def detalhe_fluxograma(request, id):
+    fluxo = get_object_or_404(Fluxograma, id=id)
+
+    dados = {
+        'id': fluxo.id,
+        'titulo': fluxo.titulo,
+        'descricao': fluxo.descricao,
+        'conteudo': fluxo.conteudo
+    }
+
+    return JsonResponse(dados)
