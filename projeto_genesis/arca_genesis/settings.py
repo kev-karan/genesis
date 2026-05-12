f6013b300cb5343bc30cffcf2af26fe04364afab
Beatriz Loyola <beatrizvasconcelos0802@gmail.com>
2026-05-12 11:10:39 -0300

    fix: adiciona Railway frontend como origem CORS

--- projeto_genesis/arca_genesis/settings.py
+++ projeto_genesis/arca_genesis/settings.py
@@ -147,6 +147,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
     'http://localhost:3000',
+    *([os.environ['FRONTEND_URL']] if os.environ.get('FRONTEND_URL') else []),
 ]
 
 # Django REST Framework
