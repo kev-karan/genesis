f0ca7b1a3034c7811acffaf393f4b43cc49c9fcc
Beatriz Loyola <beatrizvasconcelos0802@gmail.com>
2026-05-22 18:35:55 -0300

    fix: SECRET_KEY vem do .env

--- backend/arca_genesis/settings.py
+++ backend/arca_genesis/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1r6j5p0+@!rdgkc053701+n0id=!t=32qbes##t_b^zjeq3z_o'
+SECRET_KEY = os.environ['SECRET_KEY']
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
