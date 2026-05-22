1504ac45679460fbe0174be8e2d9dfc365ba9580
Beatriz Loyola <beatrizvasconcelos0802@gmail.com>
2026-05-22 18:43:48 -0300

    fix: remeve import render não usado em backend/casos/views.py

--- backend/casos/views.py
+++ backend/casos/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
